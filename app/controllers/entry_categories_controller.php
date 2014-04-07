68440f412b9bb97dca117839220c63aea5cb2048
summea <chkmyemailnow@yahoo.com>
2014-04-06 22:34:03 -0700

    formatting updates

--- app/controllers/entry_categories_controller.php
+++ app/controllers/entry_categories_controller.php
@@ -1,152 +1,132 @@
 <?php
-
 App::import('Sanitize');
 
-class EntryCategoriesController extends AppController {
-
-	var $name = 'EntryCategories';
+class EntryCategoriesController extends AppController
+{
+    var $name = 'EntryCategories';
     var $layout = 'admin';
     var $helpers = array('Cache');
     var $cacheAction = array(
         'view' => '1 day'
     );
 
-
-    function beforeFilter() {
+    function beforeFilter()
+    {
         $this->disableCache();
         parent::beforeFilter();
     }
 
-    function index() {
-
+    function index()
+    {
         // Check Permissions
         $user = $this->Session->read('Auth.User');
 
         if ($user['usertype'] <= 2) {
-
             $this->set('categories', $this->EntryCategory->find('threaded', array('order' => 'lft')));
-
         } else {
-
             $this->Session->setFlash('Sorry, permission denied.');
             $this->redirect('/home/indoors');
-
         }
-
     }
 
-    function get_updated_categories() {
-
+    function get_updated_categories()
+    {
         $this->layout = 'ajax';
 
         // Check Permissions
         $user = $this->Session->read('Auth.User');
 
         if ($user['usertype'] <= 2) {
-
             $this->set('categories', $this->EntryCategory->find('threaded', array('order' => 'lft')));
-
         } else {
-
             $this->Session->setFlash('Sorry, permission denied.');
             $this->redirect('/home/indoors');
-
         }
-
     }
 
-    function create() {
-
+    function create()
+    {
         $this->autoRender = false;
 
         // Check Permissions
         $user = $this->Session->read('Auth.User');
 
         if ($user['usertype'] <= 2) {
-
             $category_root = $this->EntryCategory->find('first', array('conditions' => array('parent_id' => null)));
             $parent_id = $category_root['EntryCategory']['id'];
-
             $new_item = array();
             $new_item['EntryCategory']['parent_id'] = $parent_id;
             $new_item['EntryCategory']['name'] = 'Untitled Category';
 
             $this->EntryCategory->save($new_item);
-
         } else {
-
             $this->Session->setFlash('Sorry, permission denied.');
             $this->redirect('/home/indoors');
-
         }
-
     }
 
-    function update() {
-    
+    function update()
+    {
         $this->autoRender = false;
 
         // Check Permissions
         $user = $this->Session->read('Auth.User');
 
         if ($user['usertype'] <= 2) {
-
-            #echo 'updating...' . '<br>';
-            #debug($_GET, true);
-            #exit;
-
             $id = Sanitize::clean($_GET['id']);
             $new_parent_id = Sanitize::clean($_GET['new_parent_id']);
             $move_up_total = Sanitize::clean($_GET['move_up_total']);
             $total_li = Sanitize::clean($_GET['total_li']);
-
             $id = str_replace('item_', '', $id);
             $new_parent_id = str_replace('item_', '', $new_parent_id);
             $move_up_total = intval($move_up_total);
             $total_li = intval($total_li);
 
-
             if ($id && $new_parent_id) {
                 $this->EntryCategory->id = $id;
                 $this->EntryCategory->save(array('parent_id' => $new_parent_id));
-                $this->EntryCategory->moveDown($id, $total_li);     # move to the bottom of the list first so we always start from bottom
+
+                // move to the bottom of the list first so we always start from bottom
+                $this->EntryCategory->moveDown($id, $total_li);
 
                 if ($move_up_total > 0)
                     $this->EntryCategory->moveUp($id, $move_up_total);
-                #else
-                #    $this->EntryCategory->moveDown($id, $total_li);     # move to the bottom of the list if needed
             }
-
         } else {
-
             $this->Session->setFlash('Sorry, permission denied.');
             $this->redirect('/home/indoors');
-
         }
-
     }
 
-    function edit($id = null) {
-
+    function edit($id = null)
+    {
         // Check Permissions
         $user = $this->Session->read('Auth.User');
 
         if ($user['usertype'] <= 2) {
-
             $id = Sanitize::clean($id);
             $this->EntryCategory->id = $id;
 
             if (empty($this->data)) {
-
                 $this->data = $this->EntryCategory->find('first',
-                                                    array(  'contain' => array( 'EntryCategoryMenuItem' => array('order' => 'weight'),
-                                                                                'EntryCategoryWidget'),
-                                                            'conditions' => array('EntryCategory.id' => $id)));
+                    array(
+                        'contain' => array(
+                            'EntryCategoryMenuItem' => array('order' => 'weight'),
+                            'EntryCategoryWidget'
+                        ),
+                        'conditions' => array('EntryCategory.id' => $id)
+                    )
+                );
 
-                $category_entry_list = $this->EntryCategory->find(  'all', array('contain' => array('Entry.id', 'Entry.subject'),
-                                                                    'conditions' => array('lft >=' => $this->data['EntryCategory']['lft'],
-                                                                                           'rght <=' => $this->data['EntryCategory']['rght']),
-                                                                    ));
+                $category_entry_list = $this->EntryCategory->find('all',
+                    array(
+                        'contain' => array('Entry.id', 'Entry.subject'),
+                        'conditions' => array(
+                            'lft >=' => $this->data['EntryCategory']['lft'],
+                            'rght <=' => $this->data['EntryCategory']['rght']
+                        ),
+                    )
+                );
 
                 $category_entry_list_array = array();
 
@@ -170,7 +150,6 @@ class EntryCategoriesController extends AppController {
                 }
                 $this->set('highest_menu_item_id', $highest_menu_item_id);
 
-
                 $dummy_item_add_label =  '<li class="menu_label" style="background-color:#aaa;">'
                                         .'<div style="float:right;"><a href="' . $this->webroot . $this->params['url']['url'] . '#delete_item" class="delete_label temporary temporary_0" onclick="return false;">Delete</a></div>'
                                         .'<div style="padding:10px;padding-top:0;">'
@@ -182,8 +161,6 @@ class EntryCategoriesController extends AppController {
                                         .'</div>'
                                         .'</div>'
                                         .'</li>';
-
-
 
                 $dummy_item_add_link =   '<li class="menu_label">'
                                         .'<div style="float:right;"><a href="' . $this->webroot . $this->params['url']['url'] . '#delete_item" class="delete_link temporary temporary_0" onclick="return false;">Delete</a></div>'
@@ -208,150 +185,116 @@ class EntryCategoriesController extends AppController {
                                         .'</div>'
                                         .'</div>'
                                         .'</li>';
-     
 
                 $this->set('dummy_item_add_label', $dummy_item_add_label);
                 $this->set('dummy_item_add_link', $dummy_item_add_link);
-
-
-
-                #echo '<br><br><br><br><br><br><pre>' . print_r($this->data, true) . '</pre>';
-                #debug($category_entry_list, true);
-                #debug($category_entry_list_array, true);
-
             } else {
-
                 // check for category menu items to delete
-
                 $delete_these_labels = split(',', $_POST['delete_these_labels']);
                 $delete_these_links = split(',', $_POST['delete_these_links']);
 
                 foreach ($delete_these_labels as $item) {
-
                     $item_id = Sanitize::escape($item);
                     $this->EntryCategory->EntryCategoryMenuItem->delete($item_id);
-
                 }
 
                 foreach ($delete_these_links as $item) {
-
                     $item_id = Sanitize::escape($item);
                     $this->EntryCategory->EntryCategoryMenuItem->delete($item_id);
-
                 }
-
 
                 if ($this->EntryCategory->saveAll($this->data)) {
-
                     $this->Session->setFlash('Your category has been updated.');
                     $this->redirect(array('action' => 'index'));
-
                 }
-
             }
-
         } else {
-
             $this->Session->setFlash('Sorry, permission denied.');
             $this->redirect('/home/indoors');
-
         }
-
     }
 
-    function rename() {
-
+    function rename()
+    {
         $this->autoRender = false;
 
         // Check Permissions
         $user = $this->Session->read('Auth.User');
 
         if ($user['usertype'] <= 2) {
-
             $id = Sanitize::clean($_GET['id']);
             $new_name = Sanitize::clean($_GET['new_name']);
-
             $id = str_replace('item_', '', $id);
 
             if (($id && $new_name) && $new_name > '') {
                 $this->EntryCategory->id = $id;
                 $this->EntryCategory->save(array('name' => $new_name));
             }
-
         } else {
-
             $this->Session->setFlash('Sorry, permission denied.');
             $this->redirect('/home/indoors');
-
         }
-
     }
 
-    function delete($id = null) {
-
+    function delete($id = null)
+    {
         $this->autoRender = false;
 
         // Check Permissions
         $user = $this->Session->read('Auth.User');
 
         if ($user['usertype'] <= 2) {
-
             $id = Sanitize::clean($id);
 
             if ($this->EntryCategory->delete($id)) {
-
                 // delete related entry_category_menu_items
                 $this->EntryCategory->query("delete from entry_category_menu_items where category_menu_id = $id");
-
                 $this->Session->setFlash('Your category has been deleted.');
                 $this->redirect(array('action' => 'index'));
-
             } else {
-
                 $this->Session->setFlash('Sorry, the category could not be deleted.');
                 $this->redirect(array('action' => 'index'));
-
             }
-
         } else {
-
             $this->Session->setFlash('Sorry, permission denied.');
             $this->redirect('/home/indoors');
-
         }
-
-
     }
 
-    function view($id = null) {
-
+    function view($id = null)
+    {
         $this->layout = 'public';
-        $website_preferences = $this->Preference->find('first');
-        $this->set('title_for_layout', $website_preferences['Preference']['website_name'] . ' : Featured Entries');
-
         $id = Sanitize::escape($id);
+        $website_preferences = $this->Preference->find('first');
+        $parent_item = $this->EntryCategory->find('first',
+            array('conditions' => array('EntryCategory.id' => $id))
+        );
+        $categories = $this->EntryCategory->find('threaded',
+            array(
+                'conditions' => array(
+                    'EntryCategory.lft >=' => $parent_item['EntryCategory']['lft'],
+                    'EntryCategory.rght <=' => $parent_item['EntryCategory']['rght']
+                ),
+                'order' => 'EntryCategory.lft'
+            )
+        );
 
+        $this->set('title_for_layout', $website_preferences['Preference']['website_name'] . ' : Featured Entries');
         $this->set('splash_images_base_url', Configure::read('splash_images_base_url'));
-
-        //$categories = $this->EntryCategory->children(1);
-
-        $parent_item = $this->EntryCategory->find('first', array('conditions' => array('EntryCategory.id' => $id)));
-
-        $categories = $this->EntryCategory->find('threaded', array( 'conditions' => array(
-                                                                        'EntryCategory.lft >=' => $parent_item['EntryCategory']['lft'],
-                                                                        'EntryCategory.rght <=' => $parent_item['EntryCategory']['rght'] ),
-                                                                    'order' => 'EntryCategory.lft' ));
-
         $this->set('categories', $categories);
 
 
         // Now we need to find featured entries for this category
         $this->EntryCategory->contain();
-        $just_categories = $this->EntryCategory->find('all', array( 'conditions' => array(
-                                                                        'EntryCategory.lft >=' => $parent_item['EntryCategory']['lft'],
-                                                                        'EntryCategory.rght <=' => $parent_item['EntryCategory']['rght'] ),
-                                                                    'order' => 'EntryCategory.lft' ));
-
+        $just_categories = $this->EntryCategory->find('all',
+            array(
+                'conditions' => array(
+                    'EntryCategory.lft >=' => $parent_item['EntryCategory']['lft'],
+                    'EntryCategory.rght <=' => $parent_item['EntryCategory']['rght']
+                ),
+                'order' => 'EntryCategory.lft'
+            )
+        );
 
         // Find what categories are within the bounds of lft and rght of current main category
         $valid_category_ids_array = array();
@@ -359,12 +302,15 @@ class EntryCategoriesController extends AppController {
             array_push($valid_category_ids_array, $item['EntryCategory']['id']);
         }
 
-
-        $featured = $this->EntryCategory->Entry->find('all', array( 'conditions' => array(
-                                                                        'Entry.cat_featured_entry' => '1',
-                                                                        'Entry.blank_page' => '0'),
-                                                                    'order' => 'Entry.id' ));
-
+        $featured = $this->EntryCategory->Entry->find('all',
+            array(
+                'conditions' => array(
+                    'Entry.cat_featured_entry' => '1',
+                    'Entry.blank_page' => '0'
+                ),
+                'order' => 'Entry.id'
+            )
+        );
 
         // Then we need to really find what entries should be featured for this particular category
         $final_featured_array = array();
@@ -374,9 +320,6 @@ class EntryCategoriesController extends AppController {
         }
 
         $this->set('featured', $final_featured_array);
-
    }
-
 }
-
 ?>
