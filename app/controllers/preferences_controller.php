3a13e73572ea20812d033f5e2fc929b446f595fe
summea <chkmyemailnow@yahoo.com>
2014-04-16 16:03:23 -0700

    formatting updates

--- app/controllers/preferences_controller.php
+++ app/controllers/preferences_controller.php
@@ -1,124 +1,77 @@
 <?php
-
-class PreferencesController extends AppController {
-
+class PreferencesController extends AppController
+{
     var $name = 'Preferences';
     var $layout = 'admin';
 
-
-    function beforeFilter() {
+    function beforeFilter()
+    {
         $this->disableCache();
         parent::beforeFilter();
     }
 
-    function list_all() {
-        return $this->Preference->find('list', array('fields' => array('Preference.name'),
-                                               'cache' => 'preferenceList'));
+    function list_all()
+    {
+        return $this->Preference->find('list',
+            array(
+                'fields' => array('Preference.name'),
+                'cache' => 'preferenceList'
+            )
+        );
     }
 
-    function index() {
-
+    function index()
+    {
         // Check Permissions
         $user = $this->Session->read('Auth.User');
 
         if ($user['usertype'] <= 3) {
-
             $this->set('title_for_layout', 'Preferences');
-
-            //$this->set('preferences', $this->Preference->find('all'));
-
         } else {
-
             $this->Session->setFlash('Sorry, permission denied.');
             $this->redirect('/home/indoors');
-
         }
-
     }
 
-    function create() {
-
+    function create()
+    {
         // Check Permissions
         $user = $this->Session->read('Auth.User');
 
         if ($user['usertype'] == 1) {
-
             $this->set('title_for_layout', 'Preferences : New');
-
         } else {
-
             $this->Session->setFlash('Sorry, permission denied.');
             $this->redirect('/home/indoors');
-
         }
-
     }
 
-    function edit() {
-/*
+    function edit()
+    {
+    }
+
+    function remove()
+    {
         // Check Permissions
         $user = $this->Session->read('Auth.User');
 
         if ($user['usertype'] == 1) {
-
-            $this->set('title_for_layout', 'Preferences : Edit');
-            
-
-            if (empty($this->data)) {
-
-                //$this->data = $this->Preference->read();
-
-            } else {
-echo '<pre>' . print_r($this->data, true) . '</pre>';
-exit;
-                //if ($this->Preference->saveAll($this->data)) {
-
-                    // clean cache for lists
-                    Cache::delete('Preference-preferenceList');
-
-                    $this->Session->setFlash('Your preference has been updated.');
-                    $this->redirect(array('action' => 'index'));
-                }
-            }
-
-        } else {
-
-            $this->Session->setFlash('Sorry, permission denied.');
-            $this->redirect('/home/indoors');
-
-        }
-*/
-    }
-
-    function remove() {
-
-        // Check Permissions
-        $user = $this->Session->read('Auth.User');
-
-        if ($user['usertype'] == 1) {
-
             $this->autorender = false;
-            
             $warnings = '';
 
             foreach ($this->data['Preference'] as $key => $value) {
                 if ($value == 1) {
-
                     $delete_this = preg_replace("/Preference|delete/", "", $key);
 
                     if ($this->Preference->delete($delete_this, false)) {
-
                         // clean cache for lists
                         Cache::delete('Preference-preferenceList');
-
                     } else {
                         $warnings .= 'Sorry, a preference couldn\'t be deleted.' . "\n";
                     }
-
                 } else {
                     // do not delete
                 }
-                
             }
 
             if ($warnings != '')
@@ -127,28 +80,21 @@ exit;
                 $this->Session->setflash('Selected preferences deleted.');
 
             $this->redirect(array('action' => 'index'));
-
-            } else {
-
+        } else {
             $this->Session->setFlash('Sorry, permission denied.');
             $this->redirect('/home/indoors');
-
         }
-
     }
 
-    function website() {
-
+    function website()
+    {
         // Check Permissions
         $user = $this->Session->read('Auth.User');
 
         if ($user['usertype'] == 1) {
-
             $this->set('title_for_layout', 'Preferences : Website');
-            
 
             if (empty($this->data)) {
-
                 $this->set('preferences', $this->Preference->find('first'));
 
                 // get current website theme!
@@ -156,77 +102,55 @@ exit;
                 $themes_array = array();
 
                 if ($handle = opendir($dir_root)) {
-
                     while (false !== ($file = readdir($handle))) {
-
                         // skip some unnecessary files
                         if ($file != '.' && $file != '..' && $file != '.DS_Store') {
                             if (is_dir($dir_root . '/' . $file))
                                 $themes_array[$file] = $file;
                         }
-
                     }
-
                 }
 
                 $this->set('website_themes', array($themes_array));
-
             } else {
-
                 if ($this->Preference->saveAll($this->data)) {
-
                     $this->Session->setFlash('Your preferences have been saved.');
                     $this->redirect(array('action' => 'index'));
-
                 } else {
-
                     $this->Session->setFlash('Sorry, there was a problem saving preferences.');
                     $this->redirect(array('action' => 'website'));
-
                 }
             }
-
         } else {
-
             $this->Session->setFlash('Sorry, permission denied.');
             $this->redirect('/home/indoors');
-
         }
-
     }
 
-    function widgets() {
-
+    function widgets()
+    {
         // Check Permissions
         $user = $this->Session->read('Auth.User');
 
         if ($user['usertype'] == 1) {
-
             $this->set('title_for_layout', 'Preferences : Website');
-            
             $this->set('preferences', $this->Preference->find('first'));
-
         } else {
-
             $this->Session->setFlash('Sorry, permission denied.');
             $this->redirect('/home/indoors');
-
         }
-
     }
 
     // clear website cache folder
-    function clear_cache() {
-
+    function clear_cache()
+    {
         $this->autoRender = false;
 
         // Check Permissions
         $user = $this->Session->read('Auth.User');
 
         if ($user['usertype'] <= 2) {
-
             if (CACHE > '') {
-
                 // sweep away old cache files because this is part of header
                 $dir = CACHE.'views'.DS;
                 if (!$dh = @opendir($dir)) return;
@@ -238,20 +162,11 @@ exit;
 
                 $this->Session->setFlash('Website cache has been cleared.');
                 $this->redirect('/preferences');
-
             }
-
         } else {
-
             $this->Session->setFlash('Sorry, permission denied.');
             $this->redirect('/preferences');
-
         }
-
-
-
     }
-
 }
-
 ?>
