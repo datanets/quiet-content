143d628ff4d8ee381fbec365747ea393b414823f
summea <chkmyemailnow@yahoo.com>
2014-04-23 11:24:24 -0700

    formatting updates

--- app/models/ad_category.php
+++ app/models/ad_category.php
@@ -1,51 +1,40 @@
 <?php
-
-class AdCategory extends AppModel {
-
-	var $name = 'AdCategory';
-
-/*
-    var $hasOne = array('AdCategoryType' => array( 'foreignKey' => 'id',
-                                            'type' => 'LEFT',
-                                            'fields' => ' ' ),
-                        'AdCategoryWidget' => array( 'foreignKey' => 'id',
-                                            'type' => 'LEFT',
-                                            'fields' => ' ' )
-                        );
-*/
-
-    var $hasMany = array('Ad' => array( 'foreignKey' => 'ad_category_id',
-                                                  'type' => 'INNER',
-                                                  'order' => 'Ad.subject',
-                                                  'conditions' => array('Ad.status_id' => '1') ));
-/*
-                         'AdCategoryMenuItem' => array( 'foreignKey' => 'category_menu_id',
-                                                  'type' => 'LEFT',
-                                                  'order' => 'AdCategoryMenuItem.weight' )
-
-                                              );
-*/
-
+class AdCategory extends AppModel
+{
+    var $name = 'AdCategory';
+    var $hasMany = array('Ad' => array(
+        'foreignKey' => 'ad_category_id',
+        'type' => 'INNER',
+        'order' => 'Ad.subject',
+        'conditions' => array('Ad.status_id' => '1')
+    ));
     var $actsAs = array('Containable', 'Tree');
 
-
-    function list_all() {
+    function list_all()
+    {
         return $this->find('list', array('order' => 'lft'));
     }
 
-    function list_nav_categories() {
-
-        $category_root_array = $this->find('first', array('conditions' => array('parent_id' => null)));
+    function list_nav_categories()
+    {
+        $category_root_array = $this->find('first',
+            array('conditions' => array('parent_id' => null))
+        );
         $category_root_id = $category_root_array['AdCategory']['id'];
 
-        $main_result_array = $this->find('all', array('conditions' => array('parent_id' => $category_root_id), 'order' => 'lft',
-                                        'contain' => array('AdCategoryMenuItem')));
-
+        $main_result_array = $this->find('all',
+            array(
+                'conditions' => array(
+                    'parent_id' => $category_root_id
+                ),
+                'order' => 'lft',
+                'contain' => array('AdCategoryMenuItem')
+            )
+        );
 
         // find all ads that are in category menus
         $category_menu_ads_list = array();
         for ($i = 0; $i < count($main_result_array); $i++) {
-
             if (isset($main_result_array[$i]['AdCategoryMenuItem']) && count($main_result_array[$i]['AdCategoryMenuItem']) > 0) {
                 foreach ($main_result_array[$i]['AdCategoryMenuItem'] as $item) {
                     if ($item['item_type'] == 2) {
@@ -53,15 +42,18 @@ class AdCategory extends AppModel {
                     }
                 }
             }
-
         }
 
-        //$category_menu_ads_string = implode(',', $category_menu_ads_list);
-
-        $ads = $this->Ad->find('all', array( 'conditions' => array(  'Ad.id' => $category_menu_ads_list,
-                                                                            'Ad.status_id' => '1'),
-                                                    'fields' => array('id', 'subject'),
-                                                    'contain' => array()));
+        $ads = $this->Ad->find('all',
+            array(
+                'conditions' => array(
+                    'Ad.id' => $category_menu_ads_list,
+                    'Ad.status_id' => '1'
+                ),
+                'fields' => array('id', 'subject'),
+                'contain' => array()
+            )
+        );
 
         $ads_array = array();
         for ($i = 0; $i < count($ads); $i++) {
@@ -74,17 +66,23 @@ class AdCategory extends AppModel {
         array_push($final_result_array, $main_result_array, $ads_array);
 
         return $final_result_array;
-
     }
 
-    function list_side_nav_categories() {
-
-        $category_root_array = $this->find('first', array('conditions' => array('parent_id' => null)));
+    function list_side_nav_categories()
+    {
+        $category_root_array = $this->find('first',
+            array('conditions' => array('parent_id' => null))
+        );
         $category_root_id = $category_root_array['AdCategory']['id'];
 
-        return $this->find('all', array('conditions' => array('parent_id' => $category_root_id), 'order' => 'lft'));
+        return $this->find('all',
+            array(
+                'conditions' => array(
+                    'parent_id' => $category_root_id
+                ),
+                'order' => 'lft'
+            )
+        );
     }
-
 }
-
 ?>
