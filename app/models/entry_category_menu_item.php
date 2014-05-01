03d9904401be0cd4b85218bc8a52b4fc08b3d9e9
summea <chkmyemailnow@yahoo.com>
2014-05-01 08:51:39 -0700

    formatting updates

--- app/models/entry_category_menu_item.php
+++ app/models/entry_category_menu_item.php
@@ -1,22 +1,16 @@
 <?php
-
-class EntryCategoryMenuItem extends AppModel {
-
-	var $name = 'EntryCategoryMenuItem';
-
+class EntryCategoryMenuItem extends AppModel
+{
+    var $name = 'EntryCategoryMenuItem';
     var $belongsTo = array(
         'EntryCategory' => array(
             'className' => 'EntryCategoryMenu',
             'foreignKey' => 'id',
-            //'foreignKey' => 'entry_category_menu_id',
             'conditions' => '',
             'fields' => '',
             'order' => ''
         )
     );
-
     var $actsAs = array('Containable');
-
 }
-
 ?>
