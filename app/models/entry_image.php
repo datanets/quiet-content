e5d91d3db9b0aec79463dc272a148aff79b5ccda
summea <chkmyemailnow@yahoo.com>
2014-05-04 00:42:48 -0700

    formatting updates

--- app/models/entry_image.php
+++ app/models/entry_image.php
@@ -1,17 +1,16 @@
 <?php
-class EntryImage extends AppModel {
-	var $name = 'EntryImage';
-	var $displayField = 'name';
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
-
-	var $belongsTo = array(
-		'Entry' => array(
-			'className' => 'Entry',
-			'foreignKey' => 'entry_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		)
-	);
+class EntryImage extends AppModel
+{
+    var $name = 'EntryImage';
+    var $displayField = 'name';
+    var $belongsTo = array(
+        'Entry' => array(
+            'className' => 'Entry',
+            'foreignKey' => 'entry_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        )
+    );
 }
 ?>
