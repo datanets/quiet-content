a80018efe6e020567aff97428fed697c20d5be92
summea <chkmyemailnow@yahoo.com>
2014-05-07 10:25:38 -0700

    formatting updates

--- app/models/mini_menu_item.php
+++ app/models/mini_menu_item.php
@@ -1,22 +1,16 @@
 <?php
-
-class MiniMenuItem extends AppModel {
-
-	var $name = 'MiniMenuItem';
-
+class MiniMenuItem extends AppModel
+{
+    var $name = 'MiniMenuItem';
     var $belongsTo = array(
         'MiniMenu' => array(
             'className' => 'MiniMenu',
             'foreignKey' => 'id',
-            //'foreignKey' => 'entry_category_menu_id',
             'conditions' => '',
             'fields' => '',
             'order' => ''
         )
     );
-
     var $actsAs = array('Containable');
-
 }
-
 ?>
