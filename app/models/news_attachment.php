c318d2effd7caaf67823845989676ac9ceeea292
summea <chkmyemailnow@yahoo.com>
2014-05-11 22:52:59 -0700

    formatting updates

--- app/models/news_attachment.php
+++ app/models/news_attachment.php
@@ -1,17 +1,16 @@
 <?php
-class NewsAttachment extends AppModel {
-	var $name = 'NewsAttachment';
-	var $displayField = 'name';
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
-
-	var $belongsTo = array(
-		'News' => array(
-			'className' => 'News',
-			'foreignKey' => 'news_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		)
-	);
+class NewsAttachment extends AppModel
+{
+    var $name = 'NewsAttachment';
+    var $displayField = 'name';
+    var $belongsTo = array(
+        'News' => array(
+            'className' => 'News',
+            'foreignKey' => 'news_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        )
+    );
 }
 ?>
