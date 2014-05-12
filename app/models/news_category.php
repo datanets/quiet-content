321d144070075385682a8a086c207ee8206b9fd9
summea <chkmyemailnow@yahoo.com>
2014-05-12 15:38:28 -0700

    formatting updates

--- app/models/news_category.php
+++ app/models/news_category.php
@@ -1,67 +1,59 @@
 <?php
-
-class NewsCategory extends AppModel {
-
-	var $name = 'NewsCategory';
-
-/*
-    var $hasOne = array('NewsCategoryType' => array( 'foreignKey' => 'id',
-                                            'type' => 'LEFT',
-                                            'fields' => ' ' ),
-                        'NewsCategoryWidget' => array( 'foreignKey' => 'id',
-                                            'type' => 'LEFT',
-                                            'fields' => ' ' )
-                        );
-*/
-
-    var $hasMany = array('News' => array( 'foreignKey' => 'news_category_id',
-                                                  'type' => 'INNER',
-                                                  'order' => 'News.subject',
-                                                  'conditions' => array('News.status_id' => '1') ));
-/*
-                         'NewsCategoryMenuItem' => array( 'foreignKey' => 'category_menu_id',
-                                                  'type' => 'LEFT',
-                                                  'order' => 'NewsCategoryMenuItem.weight' )
-
-                                              );
-*/
-
+class NewsCategory extends AppModel
+{
+    var $name = 'NewsCategory';
+    var $hasMany = array(
+        'News' => array(
+            'foreignKey' => 'news_category_id',
+            'type' => 'INNER',
+            'order' => 'News.subject',
+            'conditions' => array(
+                'News.status_id' => '1'
+            )
+        )
+    );
     var $actsAs = array('Containable', 'Tree');
 
-
-    function list_all() {
+    function list_all()
+    {
         return $this->find('list', array('order' => 'lft'));
     }
 
-    function list_nav_categories() {
-
-        $category_root_array = $this->find('first', array('conditions' => array('parent_id' => null)));
+    function list_nav_categories()
+    {
+        $category_root_array = $this->find('first', array(
+            'conditions' => array('parent_id' => null)
+        ));
         $category_root_id = $category_root_array['NewsCategory']['id'];
-
-        $main_result_array = $this->find('all', array('conditions' => array('parent_id' => $category_root_id), 'order' => 'lft',
-                                        'contain' => array('NewsCategoryMenuItem')));
-
+        $main_result_array = $this->find('all', array(
+            'conditions' => array(
+                'parent_id' => $category_root_id
+            ),
+            'order' => 'lft',
+            'contain' => array('NewsCategoryMenuItem')
+        ));
 
         // find all news that are in category menus
         $category_menu_news_list = array();
         for ($i = 0; $i < count($main_result_array); $i++) {
-
-            if (isset($main_result_array[$i]['NewsCategoryMenuItem']) && count($main_result_array[$i]['NewsCategoryMenuItem']) > 0) {
+            if (isset($main_result_array[$i]['NewsCategoryMenuItem'])
+                && count($main_result_array[$i]['NewsCategoryMenuItem']) > 0) {
                 foreach ($main_result_array[$i]['NewsCategoryMenuItem'] as $item) {
                     if ($item['item_type'] == 2) {
                         array_push($category_menu_news_list, $item['link']);
                     }
                 }
             }
-
         }
 
-        //$category_menu_news_string = implode(',', $category_menu_news_list);
-
-        $news = $this->News->find('all', array( 'conditions' => array(  'News.id' => $category_menu_news_list,
-                                                                            'News.status_id' => '1'),
-                                                    'fields' => array('id', 'subject'),
-                                                    'contain' => array()));
+        $news = $this->News->find('all', array(
+            'conditions' => array(
+                'News.id' => $category_menu_news_list,
+                'News.status_id' => '1'
+            ),
+            'fields' => array('id', 'subject'),
+            'contain' => array()
+        ));
 
         $news_array = array();
         for ($i = 0; $i < count($news); $i++) {
@@ -74,17 +66,21 @@ class NewsCategory extends AppModel {
         array_push($final_result_array, $main_result_array, $news_array);
 
         return $final_result_array;
-
     }
 
-    function list_side_nav_categories() {
-
-        $category_root_array = $this->find('first', array('conditions' => array('parent_id' => null)));
+    function list_side_nav_categories()
+    {
+        $category_root_array = $this->find('first', array(
+            'conditions' => array('parent_id' => null)
+        ));
         $category_root_id = $category_root_array['NewsCategory']['id'];
 
-        return $this->find('all', array('conditions' => array('parent_id' => $category_root_id), 'order' => 'lft'));
+        return $this->find('all', array(
+            'conditions' => array(
+                'parent_id' => $category_root_id
+            ),
+            'order' => 'lft'
+        ));
     }
-
 }
-
 ?>
