20aba26442101adc2a185fc428f4e5007cdb64d9
summea <chkmyemailnow@yahoo.com>
2014-05-13 11:18:38 -0700

    formatting updates

--- app/models/preference.php
+++ app/models/preference.php
@@ -1,16 +1,18 @@
 <?php
-
-class Preference extends AppModel {
-
+class Preference extends AppModel
+{
     var $name = 'Preference';
+    var $hasMany = array(
+        'Widget' => array(
+            'foreignKey' => 'preference_id',
+            'type' => 'INNER',
+            'order' => 'Widget.title'
+        )
+    );
 
-    var $hasMany = array('Widget' => array(   'foreignKey' => 'preference_id',
-                                              'type' => 'INNER',
-                                              'order' => 'Widget.title' ));
-
-    function afterSave() {
+    function afterSave()
+    {
         if (CACHE > '') {
-
             // sweep away old cache files because this is part of header
             $dir = CACHE.'views'.DS;
             if (!$dh = @opendir($dir)) return;
@@ -19,13 +21,12 @@ class Preference extends AppModel {
                 unlink ($dir.DS.$file);
             }
             closedir($dh);
-
         }
     }
 
-    function afterDelete() {
+    function afterDelete()
+    {
         if (CACHE > '') {
-
             // sweep away old cache files because this is part of header
             $dir = CACHE.'views'.DS;
             if (!$dh = @opendir($dir)) return;
@@ -34,10 +35,7 @@ class Preference extends AppModel {
                 unlink ($dir.DS.$file);
             }
             closedir($dh);
-
         }
     }
-
 }
-
 ?>
