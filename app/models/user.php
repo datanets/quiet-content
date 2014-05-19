9e873a7ed3ad9524ecc1f2ba50996093c6295074
summea <chkmyemailnow@yahoo.com>
2014-05-19 09:07:56 -0700

    formatting updates

--- app/models/user.php
+++ app/models/user.php
@@ -1,13 +1,13 @@
 <?php
-
-class User extends AppModel {
-
-	var $name = 'User';
-
-    var $hasOne = array('UserType' => array( 'foreignKey' => 'id',
-                                            'type' => 'LEFT',
-                                            'fields' => ' ' ));
-
+class User extends AppModel
+{
+    var $name = 'User';
+    var $hasOne = array(
+        'UserType' => array(
+            'foreignKey' => 'id',
+            'type' => 'LEFT',
+            'fields' => ' '
+        )
+    );
 }
-
 ?>
